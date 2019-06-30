c55fd3439d5192ad1520ef7eda4af2d3cd4c872e
Diego Guedes <diegoamericoguedes@gmail.com>
2019-06-30 17:45:16 -0300

    Update q7.c

--- codigos/lista_vetor/q7.c
+++ codigos/lista_vetor/q7.c
@@ -10,9 +10,11 @@ int main() {
     int A[MAX],
         B[MAX],
         AUB[MAX*2],
+        AMB[MAX], // A-B
         i,
         j,
         ind_aub,
+        ind_amb,
         tem_igual;
     
     for(i = 0; i <MAX; i++) {
@@ -26,7 +28,7 @@ int main() {
     for(i = 0; i <MAX; i++) {
         AUB[i] = A[i];
     }
-    // percorrendo B
+    // AUB = A U B
     for(i = 0,ind_aub = MAX; i < MAX; i++) {
         // verificando se o elemento i de B pertence a A
         tem_igual = 0;
@@ -41,6 +43,22 @@ int main() {
             ind_aub++;
         }
     }
+    // AMB = A - B
+    for(i = 0,ind_amb = 0; i < MAX; i++) {
+        // verificando se o elemento i de A pertence a B
+        tem_igual = 0;
+        for(j = 0; j < MAX; j++){
+            if(A[i] == B[j]) {
+                tem_igual = 1;
+                break;
+            }
+        }
+        if(tem_igual == 0) {
+            AMB[ind_amb] = A[i];
+            ind_amb++;
+        }
+    }
+    
     
     printf("Segue o vetor A:\n");
     for(i = 0; i <MAX; i++) {
@@ -55,6 +73,11 @@ int main() {
     printf("Segue o vetor AUB:\n");
     for(i = 0; i <ind_aub; i++) {
         printf("%d ",AUB[i]);
+    }
+    printf("\n");
+    printf("Segue o vetor AMB:\n");
+    for(i = 0; i <ind_amb; i++) {
+        printf("%d ",AMB[i]);
     }
     printf("\n");
 }
